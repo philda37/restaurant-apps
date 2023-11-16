521a06045f49ca28a80c5a8a4d257079d61dafce
philda37 <139132442+philda37@users.noreply.github.com>
2023-11-16 12:13:20 +0700

    Add files via upload

---
+++ restauran-app/webpack.common.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/scripts/index.js'),
+    // sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+    }),
+    new BundleAnalyzerPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
+      ],
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, 'src/public'),
+    //       to: path.resolve(__dirname, 'dist'),
+    //       globOptions: {
+    //         // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+    //         ignore: ['**/images/**'],
+    //       },
+    //     },
+    //   ],
+    // }),
+  ],
+};

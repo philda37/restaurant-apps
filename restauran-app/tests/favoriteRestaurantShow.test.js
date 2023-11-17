d94bedc6e262060ca86fa03437ff028b1b5ab5c2
philda37 <139132442+philda37@users.noreply.github.com>
2023-11-17 09:46:29 +0700

    Update favoriteRestaurantShow.test.js

--- restauran-app/tests/favoriteRestaurantShow.test.js
+++ restauran-app/tests/favoriteRestaurantShow.test.js
@@ -1,20 +1,29 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-new */
 import FavoriteRestaurantShowPresenter from '../src/scripts/views/pages/liked-restaurants/favorite-restaurant-show-presenter';
+import FavoriteRestaurantView from '../src/scripts/views/pages/liked-restaurants/favorite-restaurant-view';
 
-/* eslint-disable no-undef */
 describe('Showing all favorite restaurants', () => {
   let view;
 
   const renderTemplate = () => {
+    view = new FavoriteRestaurantView();
+    document.body.innerHTML = view.getTemplate();
   };
+
   beforeEach(() => {
     renderTemplate();
   });
 
   describe('When no restaurants have been liked', () => {
     it('should render the information that no restaurants have been liked', () => {
+      const favoriteRestaurants = {
+        getAllRestaurant: jest.fn().mockImplementation(() => []),
+      };
+
       const presenter = new FavoriteRestaurantShowPresenter({
         view,
+        favoriteRestaurants,
       });
 
       const restaurants = [];
@@ -22,25 +31,44 @@ describe('Showing all favorite restaurants', () => {
 
       expect(document.querySelectorAll('.restaurant-item__not__found').length).toEqual(1);
     });
+
     it('should ask for the favorite restaurants', () => {
       const favoriteRestaurants = {
         getAllRestaurant: jest.fn().mockImplementation(() => []),
       };
+
       new FavoriteRestaurantShowPresenter({
         view,
         favoriteRestaurants,
       });
 
-      expect(favoriteRestaurants.getAllRestaurants).toHaveBeenCalledTimes(1);
+      expect(favoriteRestaurants.getAllRestaurant).toHaveBeenCalledTimes(1);
     });
-    it('should show the information that no restaurants have been liked', (done) => {
+  });
+
+  describe('When favorite restaurants exist', () => {
+    it('should show the restaurants', (done) => {
       document.getElementById('restaurants').addEventListener('restaurants:updated', () => {
-        expect(document.querySelectorAll('.restaurant-item__not__found').length).toEqual(1);
+        expect(document.querySelectorAll('.restaurant-item').length).toEqual(2);
+
         done();
       });
 
       const favoriteRestaurants = {
-        getAllRestaurant: jest.fn().mockImplementation(() => []),
+        getAllRestaurant: jest.fn().mockImplementation(() => [
+          {
+            id: '6c7bqjgi84kcowlqdz',
+            name: 'Bring Your Phone Cafe',
+            description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
+            rating: 4.6,
+          },
+          {
+            id: 'ljx8i0qu2uckcowlqdz',
+            name: 'Run The Gun',
+            description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
+            rating: 4.6,
+          },
+        ]),
       };
 
       new FavoriteRestaurantShowPresenter({
